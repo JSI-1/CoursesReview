cf07c1ac9f08813b5d383e86fb5b1f96f66098f9
ALHUSSIN Muhammad Al-Marai <aamoo20300@gmail.com>
2025-12-19 00:12:26 +0300

    Clean up ReviewPolicy by removing docblocks
    Removed unnecessary docblocks for update and delete methods.

--- app/Policies/ReviewPolicy.php
+++ app/Policies/ReviewPolicy.php
@@ -7,17 +7,13 @@ use App\Models\User;
 
 class ReviewPolicy
 {
-    /**
-     * Determine if the user can update the review.
-     */
+   
     public function update(User $user, Review $review): bool
     {
         return $user->id === $review->user_id;
     }
 
-    /**
-     * Determine if the user can delete the review.
-     */
+   
     public function delete(User $user, Review $review): bool
     {
         return $user->id === $review->user_id;
